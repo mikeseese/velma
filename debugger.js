313ee97636c9dfd72f0303722caf9f67ed185002
Mike Seese <seesemichaelj@gmail.com>
2017-11-22 01:06:53 -0500

    start adding decoder stuff for source mapping

--- debugger.js
+++ debugger.js
@@ -1,6 +1,18 @@
 const uuidv4 = require("uuid/v4");
 const net = require("net");
-var CircularJSON = require("circular-json");
+const CircularJSON = require("circular-json");
+const fs = require("fs");
+const solc = require("solc");
+
+const SourceMappingDecoder = require("../remix/src/util/sourceMappingDecoder");
+let sourceMappingDecoder = new SourceMappingDecoder();
+
+// get .sol file
+const inputFile = process.argv[2];
+const inputContents = fs.readFileSync(inputFile).toString();
+const compilationResult1 = solc.compile(inputContents, 0);
+const sourceMap = compilationResult1.contracts[":DebugContract"].srcmapRuntime;
+const decompressedSourceMap = sourceMappingDecoder.decompressAll(sourceMap);
 
 let client = new net.Socket();
 client.connect(8455, "localhost", () => {
