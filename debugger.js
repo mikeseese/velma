0e9e9754d6acddcda78c89368258cf1b36c199b6
Mike Seese <seesemichaelj@gmail.com>
2017-12-01 00:41:47 -0500

    start cleaning up to have a real debugger

--- debugger.js
+++ debugger.js
@@ -4,25 +4,10 @@ const CircularJSON = require("circular-json");
 const fs = require("fs");
 const solc = require("solc");
 
-const EventManager = require("../remix/src/lib/eventManager");
-const TraceManager = require("../remix/src/trace/traceManager");
-const CodeManager = require("../remix/src/code/codeManager");
-const SolidityProxy = require("../remix/src/solidity/solidityProxy");
-const InternalCallTree = require("../remix/src/util/internalCallTree");
-const StepManager = require("../remix/src/ui/StepManager");
 const BreakpointManager = require("../remix/src/code/breakpointManager");
-const Web3Providers = require('../remix/src/web3Provider/web3Providers');
-const GlobalRemixUtil = require('../remix/src/helpers/global');
 const SourceMappingDecoder = require('../remix/src/util/sourceMappingDecoder');
 const CodeUtils = require('../remix/src/code/codeUtils');
 
-let currentStepIndex = -1;
-let web3Providers = new Web3Providers();
-let event = new EventManager();
-let traceManager = new TraceManager();
-let codeManager = new CodeManager(traceManager);
-let solidityProxy = new SolidityProxy(traceManager, codeManager);
-let callTree = new InternalCallTree(event, traceManager, solidityProxy, codeManager, { includeLocalVariables: true });
 let sourceMappingDecoder = new SourceMappingDecoder();
 
 // get .sol file
@@ -31,30 +16,6 @@ const inputContents = fs.readFileSync(inputFile).toString();
 const compilationResult = solc.compile(inputContents, 1);
 
 let monitoredContracts = {};
-
-codeManager.event.register('changed', this, (code, address, instIndex) => {
-  console.log("codeManager.changed(" + code + ", " + address + ", " + instIndex + ")");
-  callTree.sourceLocationTracker.getSourceLocationFromVMTraceIndex(address, currentStepIndex, solidityProxy.contracts, (error, sourceLocation) => {
-    console.log("getSourceLocationFromVMTraceIndex.callback(" + error + ", " + sourceLocation + ")");
-    if (!error) {
-      event.trigger('sourceLocationChanged', [sourceLocation]);
-    }
-  })
-});
-
-const web3ProviderType = "INTERNAL";
-web3Providers.addProvider(web3ProviderType);
-event.trigger("providerAdded", [web3ProviderType]);
-web3Providers.get(web3ProviderType, function (error, obj) {
-  if (error) {
-    console.log('provider ' + web3ProviderType + ' not defined');
-  } else {
-    GlobalRemixUtil.web3 = obj;
-    event.trigger('providerChanged', [web3ProviderType]);
-  }
-});
-solidityProxy.reset(compilationResult);
-
 
 let client = new net.Socket();
 client.connect(8455, "localhost", () => {
