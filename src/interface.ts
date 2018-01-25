c45208c4d5f8c14967e3f07c34ae87e1f8932b10
Mike Seese <seesemichaelj@gmail.com>
2018-01-25 02:29:06 -0500

    use websockets instead of nodejs sockets

--- src/interface.ts
+++ src/interface.ts
@@ -1,50 +1,47 @@
-
-import { Socket } from "net";
 import { normalize as normalizePath } from "path";
+import * as WebSocket from "ws";
 
 import { LibSdbCompile } from "./compiler";
 import { LibSdbRuntime } from "./runtime";
-import { LibSdbUtils } from "./utils";
 
 const CircularJSON = require("circular-json");
 
 export class LibSdbInterface {
-    private _socket: Socket;
+    private _wss: WebSocket.Server;
 
     private _runtime: LibSdbRuntime;
 
-    private _debuggerMessageId: string | null;
+    private _debuggerMessages: Map<string, WebSocket>;
 
     constructor(runtime: LibSdbRuntime) {
         this._runtime = runtime;
-        this._socket = new Socket();
-        this._debuggerMessageId = null;
+        this._debuggerMessages = new Map<string, WebSocket>();
     }
 
-    public respondToDebugHook(content: any = null) {
+    public respondToDebugHook(messageId: string, content: any = null) {
         // don't respond if we don't actually need to
-        if (this._debuggerMessageId === null) {
+        if (!this._debuggerMessages.has(messageId)) {
             return;
         }
 
         const response = {
             "status": "ok",
-            "id": this._debuggerMessageId,
+            "id": messageId,
             "messageType": "response",
             "content": content
         };
-        this._socket.write(CircularJSON.stringify(response));
-
-        this._debuggerMessageId = null;
+        const message = CircularJSON.stringify(response);
+        this._debuggerMessages.get(messageId)!.send(message);
+        this._debuggerMessages.delete(messageId);
     }
 
-    private socketHandler(dataSerialized: Buffer | string) {
+    private messageHandler(ws: WebSocket, message: WebSocket.Data) {
         let data;
-        if (dataSerialized instanceof Buffer) {
-            data = CircularJSON.parse(dataSerialized.toString("utf8"));
+        if (message instanceof Buffer) {
+            data = CircularJSON.parse(message.toString("utf8"));
         }
         else {
-            data = CircularJSON.parse(dataSerialized);
+            data = CircularJSON.parse(message);
         }
         const triggerType = data.triggerType;
         const messageType = data.messageType;
@@ -58,7 +55,7 @@ export class LibSdbInterface {
                 "messageType": "response",
                 "content": null
             };
-            this._socket.write(CircularJSON.stringify(response));
+            ws.send(CircularJSON.stringify(response));
         }
         else if (triggerType === "linkContractAddress") {
             const fullContractName = normalizePath(data.content.sourcePath) + ":" + data.content.contractName;
@@ -70,41 +67,32 @@ export class LibSdbInterface {
                 "messageType": "response",
                 "content": null
             };
-            this._socket.write(CircularJSON.stringify(response));
+            ws.send(CircularJSON.stringify(response));
         }
         else if (triggerType === "step") {
-            this._debuggerMessageId = data.id;
+            this._debuggerMessages.set(data.id, ws);
             this._runtime.vmStepped(data);
         }
         else if (messageType === "response") {
             if (data.content && data.content.type === "putCodeResponse") {
                 // i guess we dont care right now that this is responding to the specific request yet; we will probably eventually
-                this.respondToDebugHook(); // eek, let the debugger run!
+                this.respondToDebugHook(data.id); // eek, let the debugger run!
             }
         }
     }
 
-    public attach(host: string, port: number, callback) {
-        this._socket.on('error', function (this: LibSdbInterface, e) {
-            if (e.code === 'ECONNREFUSED') {
-                console.log('Is the server running at ' + port + '?');
+    public serve(host: string, port: number, callback) {
+        const self = this;
 
-                this._socket.setTimeout(5000, function (this: LibSdbInterface) {
-                    this._socket.connect(port, host, function () {
-                        callback();
-                    });
-                }.bind(this));
+        this._wss = new WebSocket.Server({
+            host: host,
+            port: port
+        }, callback);
 
-                console.log('Timeout for 5 seconds before trying port:' + port + ' again');
-
-            }
-        }.bind(this));
-
-        this._socket.connect(port, host, () => {
-            console.log("Connected to extension host");
-            callback();
+        this._wss.on("connection", function connection(ws: WebSocket) {
+            ws.on("message", (message) => {
+                self.messageHandler(ws, message);
+            });
         });
-
-        this._socket.on("data", this.socketHandler.bind(this));
     }
 }
